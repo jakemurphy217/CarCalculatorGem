4246d0b2145a8745d2e83f8a337c5f5c3094f3be
jakem <Hondazx123>
2020-03-26 20:02:53 +0000

    cleaning up code

--- lib/carcalculator.rb
+++ lib/carcalculator.rb
@@ -4,16 +4,19 @@ require 'carcalculator/version'
 # Carprice.greet(7+3)
 class Carprice
   def self.cal(input)
-    # puts "Hello, #{name}! I'm Ruby!"
 
-  taxrate = 0.13
+    taxrate = 0.13
+    # storing the input
+    price = input
+    # * how much the company charges for finace for the car
+    tax = price * taxrate
+    # adding the tax into the car price
+    totalprice = price + tax
 
-  # print"Enter car amount:#{name}"
+    puts "Price is $#{price}"
+    puts "Tax is $#{tax}"
+    puts "Total price is $#{totalprice}"
+    puts "Over 12 months it is $#{totalprice / 12}"
 
-  price = input
-
-  tax = price * taxrate
-
-  puts "Tax on $#{price} is $#{tax}, so your total is $#{price + tax}"
   end
 end
--- lib/carcalculator/version.rb
+++ lib/carcalculator/version.rb
@@ -1,3 +1,3 @@
 module Carcalculator
-  VERSION = "0.2.0".freeze
+  VERSION = "0.3.0".freeze
 end
